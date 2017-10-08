c54a8707d79745f0af55711909fe5aab2f152efc
Kaylin Leland <kay@thebeancountersbookkeeping.com>
2017-10-08 14:52:47 -0400

    faker

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 ruby "2.2.3"
 
+gem 'faker'
+
 gem 'devise', '4.2.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+include Faker
+
+#Create Users
+
+  5.times do
+    user = User.create!(
+    email:    Faker::Internet.email,
+    password: Faker::Internet.password
+    )
+  end
+  users = User.all
+
+# Create item
+  15.times do
+    Item.create!(
+      name: Faker::Lorem.sentence,
+    )
+  end
+  items = Item.all
+
+puts "Seed finished"
+puts "#{User.count} users created."
+puts "#{Item.count} items created." 
